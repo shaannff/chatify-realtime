c1bf44c77068107ef06476b856b451a84103ae10
shan <shaannmuhammed@gmail.com>
2026-01-09 21:38:37 +0530

    fix: correct Express route handling and prepare app for production deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 import express from "express"
 import dotenv from 'dotenv'
+import path from 'path'
+
 import authRoutes from './routes/auth.route.js'
 import messageRoute from './routes/message.route.js'
 
@@ -7,9 +9,23 @@ import messageRoute from './routes/message.route.js'
 
 dotenv.config()
 const app = express()
+
+const __dirname = path.resolve()
+
 const PORT = process.env.PORT || 3000
 
 app.use('/api/auth',authRoutes)
 app.use('/api/messages',messageRoute)
+
+if(process.env.NODE_ENV == 'production'){
+        app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+
+        app.get(/^(?!\/api).*/, (req, res) => {
+            res.sendFile(path.join(__dirname,"../frontend/dist/index.html"))
+        })
+        
+}
+
 
 app.listen(PORT,()=> console.log(`server is running http://localhost:${PORT} ` ))
