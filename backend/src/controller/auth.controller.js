43ae850fb1bb4d6bfd10c2fc00a785ca3e19c597
shan <shaannmuhammed@gmail.com>
2026-01-13 18:25:07 +0530

    login & logout functionality added

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -62,3 +62,48 @@ export const signUp = async (req,res)=>{
 
 
 };
+
+export const login = async(req, res)=>{
+
+    const {email ,password }=req.body
+    try {
+        const user = await User.findOne({email})
+        if(!user) return res.status(400).json({message:"Invalid Credentials"});
+    
+        const isPasswordCorrect = await bcrypt.compare(password,user.password)
+        if(!isPasswordCorrect) return res.status(400).json({message:"Invalid Credentials"});
+    
+        generateToken(user._id,res)
+        
+            res.status(201).json({
+                _id:user._id,
+                fullName:user.fullName,
+                email :user.email,
+                profilePic:user.profilePic
+            });
+            console.log("login succsessfully")
+
+    } catch (error) {
+        console.log("Error in login controller :",error);
+        res.status(500).json({message:"Internal server Error"})
+
+    }
+}
+export const logout = async ( req, res)=>{
+    try {
+        res.cookie("jwt", "", {
+        httpOnly: true,                 
+        expires: new Date(0),         
+        secure: process.env.NODE_ENV === "production", 
+        sameSite: "strict",             
+    });
+    res.status(200).json({message:"Logged out successfully"});
+    } catch (error) {
+        console.error("Logout Error:", error);
+        return res.status(500).json({ 
+        success: false,
+        message: "Internal Server Error" 
+    });
+    }
+
+}
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,16 +1,13 @@
 import express from 'express'
-import { signUp } from '../controller/auth.controller.js'
+import { signUp ,login, logout} from '../controller/auth.controller.js'
 
 
 const router = express.Router()
 
-router.get('/signup',signUp)
+router.post('/signup',signUp)
 
-router.get('/login',(req,res)=>{
-    res.send('login  ')
-})
-router.get('/logout',(req,res)=>{
-    res.send('logout')
-})
+router.post('/login',login)
+
+router.post('/logout',logout)
 
 export default router
